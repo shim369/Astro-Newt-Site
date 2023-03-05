d586f791d7913948f05cfd2d6d1287d67e75f3a3
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-03-05 14:59:02 +0900

    delete js file

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-let headerInner = document.querySelector(".header-inner");
-
-window.addEventListener("scroll", () => {
-	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-		headerInner.classList.add("active");
-	} else {
-		headerInner.classList.remove("active");
-	}
-})
