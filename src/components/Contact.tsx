6294a787c425cf60f5cf4299a777c49e81b55617
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-10-07 15:14:12 +0900

    test form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,23 +1,23 @@
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'
 
 type FormValues = {
-  name: string;
-  email: string;
-  message: string;
-};
+  name: string
+  email: string
+  message: string
+}
 
 export default function ContactForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>({ mode: 'onChange' });
+  } = useForm<FormValues>({ mode: 'onChange' })
 
   const onSubmit = handleSubmit(async (data) => {
-    const formData = new FormData();
+    const formData = new FormData()
     Object.entries(data).forEach(([key, value]) => {
-      formData.append(key, value);
-    });
+      formData.append(key, value)
+    })
 
     await fetch('https://shim.form.newt.so/v1/deCA6xMnK', {
       method: 'POST',
@@ -25,53 +25,55 @@ export default function ContactForm() {
       headers: {
         Accept: 'application/json',
       },
-    });
-  });
+    })
+  })
 
   return (
     <section className="bg-yellow" id="a04">
-      <div className="container">
-        <div className="contact">
-      <h1>Contact us</h1>
-          <form className="form-box" onSubmit={onSubmit}>
-            <div className="form-item">
-              <input
-                placeholder="Name"
-                id="name"
-                {...register('name', { required: 'Name is required' })}
-                aria-describedby="error-name-required"
-              />
-              {errors?.name && (
-                <span id="error-name-required" aria-live="assertive">
-                  {errors.name.message}
-                </span>
-              )}
-              <input
-                placeholder="Email"
-                id="email"
-                {...register('email', { required: 'Email is required' })}
-              />
-              {errors?.email && (
-                <span id="error-email-required" aria-live="assertive">
-                  {errors.email.message}
-                </span>
-              )}
-              <textarea
-                placeholder="Message"
-                id="message"
-                {...register('message', { required: 'Message is required' })}
-              />
-              {errors?.message && (
-                <span id="error-message-required" aria-live="assertive">
-                  {errors.message.message}
-                </span>
-              )}
-        <button type="submit">Submit</button>
+        <div className="container">
+            <div className="contact">
+            <h1>Contact</h1>
+            <form className="form-box" onSubmit={onSubmit}>
+                <div className="form-item">
+                <input
+                    placeholder="Name"
+                    id="name"
+                    {...register('name', { required: 'Name is required' })}
+                    aria-describedby="error-name-required"
+                />
+                {errors?.name && (
+                    <span id="error-name-required" aria-live="assertive">
+                    {errors.name.message}
+                    </span>
+                )}
+                <input
+                    placeholder="Email"
+                    id="email"
+                    {...register('email', { required: 'Email is required' })}
+                />
+                {errors?.email && (
+                    <span id="error-email-required" aria-live="assertive">
+                    {errors.email.message}
+                    </span>
+                )}
+                <textarea
+                    placeholder="Message"
+                    id="message"
+                    {...register('message', { required: 'Message is required' })}
+                />
+                {errors?.message && (
+                    <span id="error-message-required" aria-live="assertive">
+                    {errors.message.message}
+                    </span>
+                )}
+                <button type="submit">Submit</button>
+                </div>
+            </form>
+            <div className="contact-logo">
+                ASTRO BASE
+                </div>
+            </div>
         </div>
-      </form>
-          <div className="contact-logo">ASTRO BASE</div>
-        </div>
-      </div>
     </section>
-  );
+  )
 }
